7da1a45e6c6981108e816150198f6f5adb333579
Pritam Majumder <ptmmajumder630@gmail.com>
2021-06-25 18:38:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
       this.setState({india: users.cases_time_series[users.cases_time_series.length-1]}) //india data
       this.setState({covid: users.statewise.slice(1,users.statewise.length).sort((a, b) => b.confirmed - a.confirmed)}) //statewise data
       this.setState({vaccine: users.tested[users.tested.length-1]}) //vaccine data
-      this.setState({covidindia: users.statewise})
+      this.setState({covidindia: users.statewise}) //state data
       this.setState({dailycases: users.cases_time_series[this.state.length-1].dailyconfirmed})
     }
     );
